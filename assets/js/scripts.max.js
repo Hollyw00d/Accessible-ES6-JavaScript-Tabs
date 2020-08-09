5326100f45a02ee2caf41729750bda543eed246d
Matt Jennings <matt@mattjennings.net>
2020-08-08 20:05:36 -0700

    added max JS

--- assets/js/scripts.max.js
+++ assets/js/scripts.max.js
@@ -14,6 +14,7 @@
           matchesSelector = elem.msMatchesSelector(selector);
         } else {
           matchesSelector = elem.matches(selector);
+          console.log(matchesSelector);
         }
 
         if (matchesSelector) {
