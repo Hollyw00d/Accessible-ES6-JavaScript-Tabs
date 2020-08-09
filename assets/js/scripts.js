c5900940bd1a1ee91349a75ed50584c4bf750fad
Matt Jennings <matt@mattjennings.net>
2020-08-08 20:01:41 -0700

    fixed issue of trying to get closest() function to work

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -60,10 +60,8 @@
     function closest(elem, selector) {
       try {
        let matchesSelector; 
-       console.log(elem.msMatchesSelector(selector));
-       if(typeof elem.msMatchesSelector(selector) === 'boolean') {
+       if(elem.msMatchesSelector(selector) !== null) {
          matchesSelector = elem.msMatchesSelector(selector);  
-         console.log(matchesSelector); 
        } else {
          matchesSelector = elem.matches(selector);
        }
